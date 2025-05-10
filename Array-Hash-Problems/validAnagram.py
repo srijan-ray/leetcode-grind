3b4d967160e7d89ce1eb1b0469bdd4039d3783a3
Srijan Ray <srijanray12@gmail.com>
2025-05-10 14:49:44 -0400

    feat(validAnagram): Redo valid anagram problem in only 1 main loop

--- Array-Hash-Problems/validAnagram.py
+++ Array-Hash-Problems/validAnagram.py
@@ -5,18 +5,16 @@ class Solution:
 
         s_map: dict[str, int] = {}
 
-        for letter in s:
-            if letter in s_map:
-                s_map[letter] += 1
+        # Revision: can do these two loops in one go
+        for i in range(len(s)):
+            if s[i] in s_map:
+                s_map[s[i]] += 1
             else:
-                s_map[letter] = 1
-        for letter in t:
-            if letter not in s_map:
-                return False
-            if letter in s_map:
-                s_map[letter] -= 1
-                if s_map[letter] < 0:
-                    return False
+                s_map[s[i]] = 1
+            if t[i] in s_map:
+                s_map[t[i]] -= 1
+            else:
+                s_map[t[i]] = -1
 
         for letter in s:
             if s_map[letter] != 0:
