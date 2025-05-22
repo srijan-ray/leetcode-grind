d31792c0b3fe7604775916e0934ec04b1e9dc5d7
Srijan Ray <srijanray12@gmail.com>
2025-05-22 00:24:14 -0400

    feat: Finish valid sudoku problem

---
+++ Array-Hash-Problems/valid-sudoku.py
@@ -0,0 +1,23 @@
+from typing import List, Set
+from collections import defaultdict
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r]):
+                    return False
+                if (board[r][c] in cols[c]):
+                    return False
+                if (board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        
+        return True
