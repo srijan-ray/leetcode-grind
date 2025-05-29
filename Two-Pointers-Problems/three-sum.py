7c25ebaa3958766e0a7d5ff7681f664941516700
Srijan Ray <srijanray12@gmail.com>
2025-05-28 22:44:24 -0400

    docs(3sum): Add comments to code to make sure I undestand what I did

--- Two-Pointers-Problems/three-sum.py
+++ Two-Pointers-Problems/three-sum.py
@@ -1,28 +1,35 @@
 from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Sort to make the problem similar to two sum part 2
         nums.sort()
         
         res: List[List[int]] = []
         
         for i, a in enumerate(nums):
+            # Check for duplicate a value
             if i > 0 and a == nums[i - 1]:
                 continue
 
+            # two sum part 2 check with start starting right after a
             start = i + 1
             end = len(nums) - 1
 
             while start < end:
+                # Check if condition is met
                 if a + nums[start] + nums[end] == 0:
                     res.append([ a, nums[start], nums[end] ])
                     start += 1
                     end -= 1
 
+                    # Search for next non duplicate start value
                     while nums[start] == nums[start - 1] and start < end:
                         start += 1
 
+                # If the sum is greater than 0, then you need a smaller end number
                 elif a + nums[start] + nums[end] > 0:
                     end -= 1
+                # If the sum is less than 0, then you need a larger start number to increase the sum
                 elif a + nums[start] + nums[end] < 0:
                     start += 1
 
