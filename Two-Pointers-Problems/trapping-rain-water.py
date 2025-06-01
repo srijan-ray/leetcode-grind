2ed8e2e98d690481e013706904bc6b5daa5de637
Srijan Ray <srijanray12@gmail.com>
2025-06-01 13:24:54 -0400

    docs: added comments explaining how soln works

--- Two-Pointers-Problems/trapping-rain-water.py
+++ Two-Pointers-Problems/trapping-rain-water.py
@@ -1,6 +1,7 @@
 from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # if height is empty, return 0 for max area
         if not height:
             return 0
 
@@ -9,11 +10,14 @@ class Solution:
 
         res = 0
         while leftBound < rightBound:
+            # if leftMax is greater than right max, increment leftBound
             if leftMax < rightMax:
                 leftBound += 1
                 leftMax = max(leftMax, height[leftBound])
+                # amount of water that can be stored at that index
                 res += leftMax - height[leftBound]
             else:
+                # Repeat for right bounds
                 rightBound -= 1
                 rightMax = max(rightMax, height[rightBound])
                 res += rightMax - height[rightBound]
